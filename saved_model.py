e94cc58afd3b240c88e74b4ee1a8c1cc1c1d0f3e
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2025-07-07 10:15:18 -0300

    Update saved_model.py

--- saved_model.py
+++ saved_model.py
@@ -1,1 +1,5 @@
+from tensorflow.keras.models import load_model
+from models import dice
 
+model = load_model('models/unet_multi.h5', custom_objects={'dice': dice}, compile=False)
+model.save('models/unet_multi_tf2_compat.h5')  # reexporta
