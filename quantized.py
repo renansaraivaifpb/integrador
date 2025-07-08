ac3b0dade3908c402373c6cd1dd4804dccb6a55b
Renan Saraiva dos Santos <94909776+renansaraivaifpb@users.noreply.github.com>
2025-07-08 19:42:59 -0300

    Update quantized.py

--- quantized.py
+++ quantized.py
@@ -1,8 +1,18 @@
 import tensorflow as tf
 
-converter = tf.lite.TFLiteConverter.from_saved_model("models/unet_multi.tflite")
+def dice(y_true, y_pred, smooth=1.):
+    y_true_f = K.flatten(y_true)
+    y_pred_f = K.flatten(y_pred)
+    intersection = K.sum(y_true_f * y_pred_f)
+    return (2. * intersection + smooth) / (K.sum(y_true_f) + K.sum(y_pred_f) + smooth)
+
+
+model = tf.keras.models.load_model("models/unet_multi.h5", custom_objects={"dice": dice})  # se tiver função personalizada
+
+converter = tf.lite.TFLiteConverter.from_keras_model(model)
 converter.optimizations = [tf.lite.Optimize.DEFAULT]
-converter.target_spec.supported_types = [tf.float16]  # ou tf.int8 com calibração
+converter.target_spec.supported_types = [tf.float16]
+
 tflite_model = converter.convert()
 
 with open("modelo_otimizado.tflite", "wb") as f:
